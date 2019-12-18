b2dba0b07e1b7ec238edc6b7326580bd7f6f57ff
deeprest <soulcraftgames@gmail.com>
2019-12-18 17:04:02 -0600

    itch.io upload script

--- upload-build.sh
+++ upload-build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# unity build
-
-# push to itch.io
+zipfile=saga2020.app.zip
+rm $zipfile
+# push latest build to itch.io
 # ditto is a macos anomaly
-ditto -c -k --sequesterRsrc --keepParent saga2020.app saga2020.app.zip
-butler push saga2020.app.zip deeprest/saga2020:macos
+# pushd build/MacOS
+app=$(ls -td build/MacOS/Saga* | head -1)
+echo $app
+# popd
+ditto -c -k --sequesterRsrc --keepParent $app $zipfile
+butler push $zipfile deeprest/saga2020:macos
